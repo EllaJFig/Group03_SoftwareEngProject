a0d6d670d8a8c7b65a1fbf6e62d53b9d25a8cd20
Ella Fig <ejfig13@gmail.com>
2025-11-04 18:13:08 -0500

    Uploads csv info to Firebase database

--- database.py
+++ database.py
@@ -1,76 +1,44 @@
-import requests
-from bs4 import BeautifulSoup
+import csv
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+
+'''
+end goal --> call this function to upload a single listings attributes to a table where the feilds are the following;
+FIELDS;
+    bathrooms
+    bedrooms
+    desc
+    location
+    price
+    title
+    url
+'''
+def saveToFB(filename, fp):
+        
+        with open(filename, 'r') as file:
+            reader = csv.DictReader(file)
+            data_importing = [row for row in reader ]
+        
+        cred = credentials.Certificate(fp)
+        firebase_admin.initialize_app(cred)
+
+        db = firestore.client()
+
+        coll_ref = db.collection('listings')
+
+        for i in data_importing:
+             coll_ref.add(i)
+        
+        
 #filepath to key
 fp = r"/Users/ejfig/Desktop/group03softengproj-firebase-adminsdk-fbsvc-6dc0225bd0.json"
+filename = "kijiji_data.csv" #maybe we change this to work with both files
 
-'''testing data
-data = {
-    'title': 'testing123',
-    'address': 'thisisatest'
-}
-'''
 
-#setting data into database --> doc_ref.set(data)
-
-# printing ID --> print("Document ID: ", doc_ref.id)
-
-class StoreData():
-   
-
-    '''
-    end goal --> call this function to upload a single listings attributes to a table where the feilds are the following;
-    FIELDS;
-        title
-        price
-        address
-        lat
-        long
-        bedrooms
-        bathrooms
-        desc
-        area 
-        petFriendly
-        listingURL
-        source 
-        type
-    
-    
-    def saveToFB(listing):
-        pass
-    '''
-
-def scrapTest():
-    fp = r"/Users/ejfig/Desktop/group03softengproj-firebase-adminsdk-fbsvc-6dc0225bd0.json"
-
-        #connection to firebase database
-    cred = credentials.Certificate(fp)
-    firebase_admin.initialize_app(cred)
-
-    #pointer
-    db = firestore.client()
-
-    url = "https://www.kijiji.ca/b-real-estate/canada/c34l0?view=list"
-    res = requests.get(url)
-    soup = BeautifulSoup(res.content, 'lxml')
-
-    scr_data = []
-    scr_data.append({'title': 'testing', 'source_url': 'abcd.com'})
-    
-    for h in soup.find_all('all'):
-        scr_data.append({'title': h.text.strip(), 'source_url':url})
-
-    #adding to collection
-    doc_ref = db.collection('listingsCollection').document()
-
-    for i in scr_data:
-        doc_ref.set(i)
-
-scrapTest()
+saveToFB(filename, fp)
 
 
 
