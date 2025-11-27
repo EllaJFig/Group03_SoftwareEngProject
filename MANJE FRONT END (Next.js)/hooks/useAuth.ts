0693e87dd5e54c66d9f04846d6b902d23f612ddc
noahsamarita <144628875+noahsamarita@users.noreply.github.com>
2025-11-27 17:24:01 -0500

    Add files via upload

---
+++ MANJE FRONT END (Next.js)/hooks/useAuth.ts
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { auth } from "@/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+
+export function useAuth() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoading(false);
+    });
+
+    return () => unsub();
+  }, []);
+
+  return { user, loading };
+}
